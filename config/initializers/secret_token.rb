216c524e72918ca071d1bf39e282d2f15ed11155
simonhukin <simonhukin@gmail.com>
2011-05-02 10:06:21 +1000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Eightacross::Application.config.secret_token = '8e9f77edbf0a78b37af3308b8205fb121a577c2a17670f2e124f06a1add33d3ae2e5af9a98a72f93177b2170d7f7b9fe89e97502996e33aed8f3bc70f8d6f8fd'
